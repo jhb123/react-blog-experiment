da7e36ba8e001925bfd6d0ec9071eb14158719c0
jhb123 <jhbriggs23@gmail.com>
2023-09-27 10:03:10 +0100

    made a context that lets you sign in and out

--- frontend/src/components/AdminForm.js
+++ frontend/src/components/AdminForm.js
@@ -1,6 +1,7 @@
 
-import { useState } from "react";
+import { useState, useContext } from "react";
 import axios from "axios";
+import { AdminContext } from "../pages/Layout";
 
 const LabeledField = ({type, name, label, onChange}) => {
   return (
@@ -13,39 +14,42 @@ const LabeledField = ({type, name, label, onChange}) => {
 
 
 const AdminForm = ({handleAdminToggle, handleToggleShowAdminPage}) => {
+  
   const [password, setPassword] = useState("")
-    const handleSetPassword = (event) => {
-      setPassword((password) => event.target.value)
-    }
+  const handleSetPassword = (event) => {
+    setPassword((password) => event.target.value)
+  }
 
   const [isErr, setIsErr] = useState(false)
-    
 
-  const admin_req = (input_password) => axios.post('/admin_login', {
-    password: input_password,
-  }).then(function (response) {
-    handleAdminToggle(true);
-    handleToggleShowAdminPage();
-    setIsErr(false);
-  })
-  .catch(function (error) {
-    console.log(error);
-    setIsErr(true);
-  });
+  const [isAdmin, setIsAdmin] = useContext(AdminContext);
 
+
+  const admin_req = (input_password) => 
+    axios.post('/admin_login', {password: input_password})
+      .then(function (response) {
+        handleToggleShowAdminPage();
+        setIsAdmin(true);
+        setIsErr(false);
+      })
+      .catch(function (error) {
+        console.log("there was an issue");
+        setIsAdmin(false);
+        setIsErr(true);
+      });
 
 
   return (
     <div className="scrim">
-        <div className="admin-control">
-          <div className="admin-form">
-          <LabeledField label="Admin" type="text" name = "password" onChange={handleSetPassword}/>
-          { isErr ? <p1>Error logging in</p1> : null}
-          <button onClick={() => admin_req(password)}>sign in</button>
-          <button onClick={handleToggleShowAdminPage}>cancel</button>
-          </div>
+      <div className="admin-control">
+        <div className="admin-form">
+        <LabeledField label="Admin" type="text" name = "password" onChange={handleSetPassword}/>
+        { isErr ? <p1>Error logging in</p1> : null}
+        <button onClick={() => admin_req(password)}>sign in</button>
+        <button onClick={handleToggleShowAdminPage}>cancel</button>
         </div>
-     </div>
+      </div>
+    </div>
   );
 };
 
