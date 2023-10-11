f57b67f232f4001f669b6451814673625080d451
jhb123 <jhbriggs23@gmail.com>
2023-10-12 00:14:50 +0100

    refactor more

--- frontend/src/components/EditorToolbarPlugin.js
+++ frontend/src/components/EditorToolbarPlugin.js
@@ -4,14 +4,18 @@ import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext
 import {
     $isRangeSelection, 
     $getSelection,
-
+    $createParagraphNode,
 } from 'lexical';
+import { $setBlocksType } from '@lexical/selection';
+import { $createHeadingNode, $createQuoteNode } from '@lexical/rich-text';
 import { mergeRegister } from '@lexical/utils';
 
 
 
 import Box from '@mui/material/Box';
 import { ButtonGroup } from "@mui/material";
+import Button from '@mui/material/Button';
+import LinkIcon from '@mui/icons-material/Link';
 
 import { BoldToggle, CodeToggle, ItalicToggle, UnderlineToggle } from "./editor-toggles"
 import { InsertImageTest } from './ImagePlugin';
@@ -61,6 +65,29 @@ function EditorToolbarPlugin() {
             </ButtonGroup>
         </Box>
     );
+}
+
+function LinkButton() {
+    const [editor] = useLexicalComposerContext();
+    const applyAction = (item) => {
+        editor.update(() => {
+          const selection = $getSelection();
+          if ($isRangeSelection(selection)) {
+            if(item === 'p'){
+                $setBlocksType(selection, () => $createParagraphNode(item));
+            }
+            else {
+                $setBlocksType(selection, () => $createHeadingNode(item));
+            }
+          }
+        });
+      };
+
+      return (
+        <Button >
+            <LinkIcon />
+        </Button>
+      )
 }
 
 export default EditorToolbarPlugin
--- frontend/src/components/editor.js
+++ frontend/src/components/editor.js
@@ -1,60 +1,24 @@
 import "./styles.css"
-import { useEffect, useState, useCallback, useRef} from 'react';
-import Image from 'react'
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
 import { ContentEditable } from '@lexical/react/LexicalContentEditable';
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
-import { AutoLinkPlugin } from '@lexical/react/LexicalAutoLinkPlugin';
 import { LinkPlugin } from '@lexical/react/LexicalLinkPlugin';
 
 import { TabIndentationPlugin } from '@lexical/react/LexicalTabIndentationPlugin';
 
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
-import {
-    $isRangeSelection, 
-    $getSelection,
-    FORMAT_TEXT_COMMAND,
-    FORMAT_ELEMENT_COMMAND,
-    $createParagraphNode,
-    $createTextNode,
-    $insertNodes,
-    createCommand,
-    DecoratorNode,
-    COMMAND_PRIORITY_LOW,
-} from 'lexical';
-import { mergeRegister } from '@lexical/utils';
-import { $setBlocksType } from '@lexical/selection';
-import { $createHeadingNode, $createQuoteNode } from '@lexical/rich-text';
 import { HeadingNode } from '@lexical/rich-text';
 import { TreeView } from "@lexical/react/LexicalTreeView";
 import { AutoLinkNode, LinkNode } from "@lexical/link"
 
 
-import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import FormatBoldIcon from '@mui/icons-material/FormatBold';
-import FormatItalicIcon from '@mui/icons-material/FormatItalic';
-import FormatSize from '@mui/icons-material/FormatSize';
-import FormatCodeIcon from '@mui/icons-material/Code';
-import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined';
-import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
-import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
-import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
-import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
-import LinkIcon from '@mui/icons-material/Link';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import { ButtonGroup } from "@mui/material";
 
-import { BoldToggle, CodeToggle, ItalicToggle, UnderlineToggle } from "./editor-toggles"
 import EditorToolbarPlugin from "./EditorToolbarPlugin";
 import { ImageNode } from "./ImagePlugin";
 
-import landscape from "../images/large.jpeg"
 
 const theme = {
     heading: {
@@ -113,38 +77,6 @@ function Editor() {
     );
 }
 
-/*--------------------------- Image test ---------------------------*/
-
-
-/* -------------------------- link ----------------------------------*/
-
-function LinkButton() {
-    const [editor] = useLexicalComposerContext();
-    const applyAction = (item) => {
-        editor.update(() => {
-          const selection = $getSelection();
-          if ($isRangeSelection(selection)) {
-            if(item === 'p'){
-                $setBlocksType(selection, () => $createParagraphNode(item));
-            }
-            else {
-                $setBlocksType(selection, () => $createHeadingNode(item));
-            }
-          }
-        });
-      };
-
-      return (
-        <Button >
-            <LinkIcon />
-        </Button>
-      )
-}
-
-/*--------------------------- fancy menus ---------------------------*/
-
-
-
 
 function TreeViewPlugin() {
   const [editor] = useLexicalComposerContext();
@@ -159,11 +91,6 @@ function TreeViewPlugin() {
     />
   );
 }
-
-/*--------------------------- image node ---------------------------*/
-
-
-
 
 
 export default Editor
