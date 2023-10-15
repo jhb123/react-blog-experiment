447e3678135770b1dec182f9698fd0a893bd5c04
jhb123 <jhbriggs23@gmail.com>
2023-10-15 09:41:51 +0100

    reformat

--- frontend/src/components/ImagePlugin.js
+++ frontend/src/components/ImagePlugin.js
@@ -1,120 +1,120 @@
 import "./styles.css"
-import { useRef} from 'react';
+import { useRef } from 'react';
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
 
 
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import {
-    $insertNodes,
-    createCommand,
-    DecoratorNode,
-    COMMAND_PRIORITY_LOW,
+  $insertNodes,
+  createCommand,
+  DecoratorNode,
+  COMMAND_PRIORITY_LOW,
 } from 'lexical';
 
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
-export function InsertImageTest () {
-    
-    const INSERT_IMAGE_COMMAND = createCommand();
+export function InsertImageTest() {
 
-    const [editor] = useLexicalComposerContext();
+  const INSERT_IMAGE_COMMAND = createCommand();
 
-    editor.registerCommand(
-        INSERT_IMAGE_COMMAND,
-        (payload) => {
-          const imageNode = $createImageNode("image", payload);
-          $insertNodes([imageNode]);
-          return false;
-        },
-        COMMAND_PRIORITY_LOW,
-      );
+  const [editor] = useLexicalComposerContext();
 
-    const selectImage = () => {
-        // return( <Button>New</Button>);
+  editor.registerCommand(
+    INSERT_IMAGE_COMMAND,
+    (payload) => {
+      const imageNode = $createImageNode("image", payload);
+      $insertNodes([imageNode]);
+      return false;
+    },
+    COMMAND_PRIORITY_LOW,
+  );
+
+  const selectImage = () => {
+    // return( <Button>New</Button>);
+  }
+  const inputFile = useRef(null)
+
+  const onClick = () => {
+    // selectImage()
+    inputFile.current.click();
+    // editor.dispatchCommand(INSERT_IMAGE_COMMAND,"test payload");
+  }
+
+  const handleFileChange = event => {
+    const fileObj = event.target.files && event.target.files[0];
+    if (!fileObj) {
+      return;
     }
-    const inputFile = useRef(null) 
 
-    const onClick = () => {
-        // selectImage()
-        inputFile.current.click();
-        // editor.dispatchCommand(INSERT_IMAGE_COMMAND,"test payload");
-    }
+    event.target.value = null;
 
-    const handleFileChange = event => {
-        const fileObj = event.target.files && event.target.files[0];
-        if (!fileObj) {
-          return;
-        }
-        
-        event.target.value = null;
-            
-        const reader = new FileReader();
-        reader.addEventListener("load", function () {
-            // convert image file to base64 string and save to localStorage
-            localStorage.setItem(fileObj.name,reader.result);
-            editor.dispatchCommand(INSERT_IMAGE_COMMAND,fileObj.name);
+    const reader = new FileReader();
+    reader.addEventListener("load", function () {
+      // convert image file to base64 string and save to localStorage
+      localStorage.setItem(fileObj.name, reader.result);
+      editor.dispatchCommand(INSERT_IMAGE_COMMAND, fileObj.name);
 
-        }, false);
+    }, false);
 
-        reader.readAsDataURL(fileObj);
-        
+    reader.readAsDataURL(fileObj);
 
-      };
 
-    return (
-        <Button onClick={onClick} color={"tool"}>
-            <Typography>Insert Image</Typography>
-            <input 
-                type='file' 
-                id='file' 
-                ref={inputFile} 
-                style={{display: 'none'}}
-                onChange={handleFileChange}
-                accept="image/png, image/jpeg"/>
-        </Button>
-    )
+  };
+
+  return (
+    <Button onClick={onClick} color={"tool"}>
+      <InsertPhotoIcon />
+      <input
+        type='file'
+        id='file'
+        ref={inputFile}
+        style={{ display: 'none' }}
+        onChange={handleFileChange}
+        accept="image/png, image/jpeg" />
+    </Button>
+  )
 }
 
 export class ImageNode extends DecoratorNode {
-    // essentially copied from the documentation
+  // essentially copied from the documentation
 
-    __id;
-  
-    static getType() {
-      return 'image';
-    }
-  
-    static clone(node) {
-      return new ImageNode(node.__id, node.__fname , node.__key);
-    }
-  
-    constructor(id, imgData, key) {
-      super(key);
-      this.__id = id;
-      this.__fname = imgData;
-      this.__imgData = localStorage.getItem(imgData);
-    }
-  
-    createDOM() {
-        // this puts the image in a span. This feels like a nicer way to edit the document
-        return document.createElement('span');
-    }
-  
-    updateDOM() {
-      return false;
-    }
-  
-    decorate() {
-        // temporary image to test the editor.
-        console.log("drawing " + this.__fname)
-        return <img className="editor-image" src={this.__imgData} alt="Loch Lomond"/>;
-    }
+  __id;
+
+  static getType() {
+    return 'image';
   }
-  
-  // these probably dont need exporting
-  export function $createImageNode(id, imgData) {
-    return new ImageNode(id, imgData);
+
+  static clone(node) {
+    return new ImageNode(node.__id, node.__fname, node.__key);
   }
-  export function $isImageNode(node) {
-    return node instanceof ImageNode;
+
+  constructor(id, imgData, key) {
+    super(key);
+    this.__id = id;
+    this.__fname = imgData;
+    this.__imgData = localStorage.getItem(imgData);
   }
+
+  createDOM() {
+    // this puts the image in a span. This feels like a nicer way to edit the document
+    return document.createElement('span');
+  }
+
+  updateDOM() {
+    return false;
+  }
+
+  decorate() {
+    // temporary image to test the editor.
+    console.log("drawing " + this.__fname)
+    return <img className="editor-image" src={this.__imgData} alt="Loch Lomond" />;
+  }
+}
+
+export function $createImageNode(id, imgData) {
+  return new ImageNode(id, imgData);
+}
+export function $isImageNode(node) {
+  return node instanceof ImageNode;
+}
