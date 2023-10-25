c3bf53e2b377ca3023aa15d0279b75c903acfc5c
jhb123 <jhbriggs23@gmail.com>
2023-10-25 21:08:49 +0100

    make test layout

--- frontend/src/pages/Blogs.js
+++ frontend/src/pages/Blogs.js
@@ -1,52 +1,57 @@
+import { useRef, useState, useEffect } from 'react';
+
 import Editor from "../components/editor";
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
+import Paper from '@mui/material/Paper';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import placeHolder from "../images/large.jpeg"
-import placeHolder2 from "../images/test.png"
-
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import placeHolder from "../images/large.jpeg"
+import placeHolder2 from "../images/test.png"
 
 const Blogs = () => {
 
     const cardData1 = [placeHolder,"Loch Lomond", "blurb"]
     const cardData2 = [placeHolder,"Bonnie Bonnie banks of Loch Lomond", "blurb"]
     const cardData3 = [placeHolder2,"Loch Lomond", "\
-    By yon bonnie banks and by yon bonnie braes,\
-Where the sun shines bright on Loch Lomond,\
+      By yon bonnie banks and by yon bonnie braes,\
+  Where the sun shines bright on Loch Lomond,\
 Where me and my true love were ever wont to gae,\
-On the bonnie, bonnie banks o' Loch Lomond.\
-\
-Chorus:\
-O ye'll tak' the high road, and I'll tak' the low road,\
-And I'll be in Scotland afore ye,\
-But me and my true love will never meet again,\
-On the bonnie, bonnie banks o' Loch Lomond.\
-\
-    "]
+  On the bonnie, bonnie banks o' Loch Lomond.\
+  \
+  Chorus:\
+  O ye'll tak' the high road, and I'll tak' the low road,\
+  And I'll be in Scotland afore ye,\
+  But me and my true love will never meet again,\
+  On the bonnie, bonnie banks o' Loch Lomond.\
+  \
+      "]
+
+    const testCards = [cardData1,cardData2, cardData3]
+
+    const [cards, setCards] = useState(testCards );
+
+    const addCard = () => {
+      var item = testCards[Math.floor(Math.random()*testCards.length)];
+      setCards(cards => [item,...cards] );
+    }
 
     return (
     <>
       <h1>Blog Articles</h1>
       <Grid container spacing={2} alignItems="stretch">
         <Grid item xs={4}>
-          <Button variant="contained">Create</Button>
+          <AdminControlPanel addCard={addCard}>
+          </AdminControlPanel>
         </Grid>
-        <Grid item xs={4}>
-          <BlogCard image={cardData1[0]} title = {cardData1[1]} blurb={cardData1[2]}></BlogCard>
-        </Grid>
-        <Grid item xs={4}>
-          <BlogCard image={cardData2[0]} title = {cardData2[1]} blurb={cardData2[2]}></BlogCard>
-        </Grid>
-        <Grid item xs={4}>
-          <BlogCard image={cardData3[0]} title = {cardData2[1]} blurb={cardData3[2]}></BlogCard>
-        </Grid>
-        <Grid item xs={4}>
-          <BlogCard image={cardData1[0]} title = {cardData1[1]} blurb={cardData1[2]}></BlogCard>
-        </Grid>
+        {cards.map((item,index) =>
+          <Grid item xs={4}>
+            <BlogCard image={item[0]} title = {item[1]} blurb={item[2]}></BlogCard>
+          </Grid>
+        )}
       </Grid>
 
       <Editor/>
@@ -54,12 +59,30 @@ On the bonnie, bonnie banks o' Loch Lomond.\
     )
   };
   
+  const AdminControlPanel = ({addCard}) => {
+    return(
+      <Paper sx={{
+        background: "#aaaaaa",
+        display: "flex", 
+        flexDirection:"column", 
+        alignItems: "center",
+        justifyContent: "space-around",
+        maxWidth: 345, 
+        height:"100%", 
+        p:2 
+        }}>
+        <Typography color="tool" variant='h5'>Admin Controls</Typography>
+        <Button color="tool" variant="contained" onClick={addCard}>Create</Button>
+      </Paper>
+    )
+  }
 
   const BlogCard = ({image, title, blurb}) => {
 
     return(
       <Card sx={{ 
           maxWidth: 345, 
+          minHeight: 345, 
           height:"100%", 
           display:"flex", 
           flexDirection: "column", 
